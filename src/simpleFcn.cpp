5e53913f4c3ee333178d9e08ff341d00927637db
shubham1637 <shubham.1637@gmail.com>
2019-05-27 22:24:58 -0400

    Correct quantile calculation, indexing was incorrect

--- src/simpleFcn.cpp
+++ src/simpleFcn.cpp
@@ -18,12 +18,10 @@ double getQuantile(std::vector<double> vec, double quantile){
   int j = floor(n*p + m);
   double g = n*p + m - j; // Replacing n*p with 24884.16 outputs correct result.
   double gamma = g;
-  // TODO : Precision and n_th element
-  // long double jh = 24884.16;
-  // Rcpp::Rcout << "g  = " << g << std::endl;
-  //Rcpp::Rcout << "jh  = " << std::setprecision(8) << (double)n*(double)p + m - j << std::endl;
-  //std::nth_element(vec.begin(), vec.begin()+1, vec.end(), std::greater<double>());
+  // TODO : n_th element
+  // Rcpp::Rcout << "jh  = " << std::setprecision(8) << (double)n*p + m - j << std::endl;
+  // std::nth_element(vec.begin(), vec.begin()+1, vec.end(), std::greater<double>());
   sort(vec.begin(), vec.end());
-  double sampleQuant = (1.0 - gamma)*vec[j] + gamma*vec[j+1];
+  double sampleQuant = (1.0 - gamma)*vec[j-1] + gamma*vec[j];
   return sampleQuant;
 }
