f99d1c88dd56af10da10fa179db6fdd1f34c5f0b
Hannes Roest <hannes.rost@utoronto.ca>
2019-06-15 14:52:48 -0400

    [FEATURE] cleanup, use copy elision

--- src/run_alignment.cpp
+++ src/run_alignment.cpp
@@ -76,9 +76,9 @@ ChromData readFile(std::string inputfile)
 }
 
 AffineAlignObj alignChromatogramsCpp(
-                        const std::vector<std::vector<double> > & r1,
-                        const std::vector<std::vector<double> > & r2,
-                        std::string alignType,
+                            const std::vector<std::vector<double> > & r1,
+                            const std::vector<std::vector<double> > & r2,
+                            std::string alignType,
                             const std::vector<double>& tA, const std::vector<double>& tB,
                             const std::string & normalization, const std::string& simType,
                             double B1p = 0.0, double B2p =0.0, int noBeef = 0,
@@ -89,10 +89,7 @@ AffineAlignObj alignChromatogramsCpp(
 {
 
   SimMatrix s = getSimilarityMatrix(r1, r2, normalization, simType, cosAngleThresh, dotProdThresh);
-  std::cout << " got sim mat " << std::endl;
-  AffineAlignObj obj(s.n_row+1, s.n_col+1); // Initializing C++ AffineAlignObj struct
   double gapPenalty = getGapPenalty(s, gapQuantile, simType);
-  std::cout << " gap penalty " << gapPenalty << std::endl;
   if (alignType == "hybrid")
   {
     SimMatrix MASK;
@@ -101,14 +98,12 @@ AffineAlignObj alignChromatogramsCpp(
     MASK.data.resize(MASK.n_row*MASK.n_col, 0.0);
     double A1 = tA[0], A2 = tA[MASK.n_row-1];
     double B1 = tB[0], B2 = tB[MASK.n_col-1];
-    std::cout << " mask? " << std::endl;
     calcNoBeefMask(MASK, A1, A2, B1, B2, B1p, B2p, noBeef, hardConstrain);
     auto maxIt = max_element(std::begin(s.data), std::end(s.data));
     double maxVal = *maxIt;
-    std::cout << " constrain? " << std::endl;
     constrainSimilarity(s, MASK, -2.0*maxVal/samples4gradient);
   }
-  obj = doAffineAlignment(s, gapPenalty*goFactor, gapPenalty*geFactor, OverlapAlignment); // Performs alignment on s matrix and returns AffineAlignObj struct
+  AffineAlignObj obj = doAffineAlignment(s, gapPenalty*goFactor, gapPenalty*geFactor, OverlapAlignment); // Performs alignment on s matrix and returns AffineAlignObj struct
   getAffineAlignedIndices(obj); // Performs traceback and fills aligned indices in AffineAlignObj struct
   return obj;
 }
