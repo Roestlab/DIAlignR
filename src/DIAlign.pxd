7ac7312778a6c705360f532662f5ce7f27151266
Hannes Roest <hannes.rost@utoronto.ca>
2019-06-18 20:21:02 -0400

    [FEATURE] Python bindings

---
+++ src/DIAlign.pxd
@@ -0,0 +1,72 @@
+# distutils: language = c++
+"""
+The following provides a Python wrapper around the C++ DIAlign library
+
+
+	DIAlign.pxd
+        hannes.rost@utoronto.ca
+ 
+	Copyright 2019 Hannes Roest
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from libcpp.vector cimport vector as libcpp_vector
+from libcpp.string cimport string as libcpp_string
+from libcpp cimport bool
+
+cdef extern from "utils.h" namespace "DIAlign":
+
+    double getQuantile(libcpp_vector[double] vec, double quantile) nogil except +
+
+cdef extern from "affinealignobj.h" namespace "DIAlign":
+
+    cdef cppclass AffineAlignObj:
+        AffineAlignObj(int row, int col) nogil except +
+        AffineAlignObj(AffineAlignObj o) nogil except +
+
+        libcpp_vector[int] indexA_aligned
+        libcpp_vector[int] indexB_aligned
+        libcpp_vector[double] score
+
+cdef extern from "CppInterface.hpp" namespace "DIAlign":
+
+    void alignChromatogramsCpp( AffineAlignObj& obj,
+                                libcpp_vector[libcpp_vector[double] ] & r1,
+                                libcpp_vector[libcpp_vector[double] ] & r2,
+                                libcpp_string alignType,
+                                libcpp_vector[double]& tA,
+                                libcpp_vector[double]& tB,
+                                libcpp_string & normalization,
+                                libcpp_string simType) nogil except +
+
+    void alignChromatogramsCpp( AffineAlignObj& obj,
+                                libcpp_vector[libcpp_vector[double] ] & r1,
+                                libcpp_vector[libcpp_vector[double] ] & r2,
+                                libcpp_string alignType,
+                                libcpp_vector[double]& tA,
+                                libcpp_vector[double]& tB,
+                                libcpp_string & normalization,
+                                libcpp_string simType,
+                                double B1p,
+                                double B2p,
+                                int noBeef,
+                                double goFactor,
+                                double geFactor,
+                                double cosAngleThresh,
+                                bool OverlapAlignment,
+                                double dotProdThresh,
+                                double gapQuantile,
+                                bool hardConstrain,
+                                double samples4gradient)
+
---
+++ src/PyDIAlign.pyx
@@ -0,0 +1,99 @@
+#cython: embedsignature=True
+#distutils: language = c++
+"""
+A Python wrapper around the C DIAlign library
+"""
+
+import numpy as np
+cimport numpy as np
+
+from libcpp.vector cimport vector as libcpp_vector
+from libcpp cimport bool
+from cython.operator cimport dereference as deref, preincrement as inc, address as address
+from DIAlign cimport getQuantile as _getQuantile
+from DIAlign cimport AffineAlignObj as _AffineAlignObj
+from libcpp.memory cimport shared_ptr
+from DIAlign cimport alignChromatogramsCpp as _alignChromatogramsCpp
+
+def getQuantile(data, q):
+    """
+        Compute quantile q on the data
+        
+    """
+    cdef libcpp_vector[double] c_data = data
+    cdef double result = _getQuantile( c_data, q)
+    return result
+
+cdef class AffineAlignObj:
+    """
+    Cython implementation of _AffineAlignObj
+    """
+    cdef shared_ptr[_AffineAlignObj] inst
+
+    def __dealloc__(self):
+         self.inst.reset()
+
+    def __init__(self, int row, int col):
+        """Cython signature: void AffineAlignObj()"""
+        self.inst = shared_ptr[_AffineAlignObj](new _AffineAlignObj(row, col))
+
+    property score:
+        def __set__(self, np.ndarray[double, ndim=1, mode="c"] score not None):
+            self.inst.get().score = score
+    
+        def __get__(self):
+            return self.inst.get().score
+
+    property indexA_aligned:
+        def __set__(self, np.ndarray[double, ndim=1, mode="c"] indexA_aligned not None):
+            self.inst.get().indexA_aligned = indexA_aligned
+    
+        def __get__(self):
+            return self.inst.get().indexA_aligned
+
+    property indexB_aligned:
+        def __set__(self, np.ndarray[double, ndim=1, mode="c"] indexB_aligned not None):
+            self.inst.get().indexB_aligned = indexB_aligned
+    
+        def __get__(self):
+            return self.inst.get().indexB_aligned
+
+def alignChromatogramsCppSimple(AffineAlignObj obj,
+                                np.ndarray[double, ndim=2, mode="c"] r1 not None,
+                                np.ndarray[double, ndim=2, mode="c"] r2 not None,
+                                bytes alignType,
+                                np.ndarray[double, ndim=1, mode="c"] tA not None,
+                                np.ndarray[double, ndim=1, mode="c"] tB not None,
+                                bytes normalization,
+                                bytes simType):
+    
+    _alignChromatogramsCpp(deref(obj.inst), r1, r2, alignType, tA, tB, normalization, simType)
+
+
+
+def alignChromatogramsCpp(      AffineAlignObj obj,
+                                np.ndarray[double, ndim=2, mode="c"] r1 not None,
+                                np.ndarray[double, ndim=2, mode="c"] r2 not None,
+                                bytes alignType,
+                                np.ndarray[double, ndim=1, mode="c"] tA not None,
+                                np.ndarray[double, ndim=1, mode="c"] tB not None,
+                                bytes normalization,
+                                bytes simType,
+                                double B1p,
+                                double B2p,
+                                int noBeef,
+                                double goFactor,
+                                double geFactor,
+                                double cosAngleThresh,
+                                bool OverlapAlignment,
+                                double dotProdThresh,
+                                double gapQuantile,
+                                bool hardConstrain,
+                                double samples4gradient):
+    
+    _alignChromatogramsCpp(deref(obj.inst), r1, r2, alignType, tA, tB, normalization, simType,
+                                B1p, B2p, noBeef, goFactor, geFactor, cosAngleThresh, OverlapAlignment,
+                                dotProdThresh, gapQuantile, hardConstrain, samples4gradient)
+
+
+
