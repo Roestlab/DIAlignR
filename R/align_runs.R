b7a08cdd6b475c80a33507f62be29c84ec5954b5
shubham1637 <shubham.1637@gmail.com>
2019-10-16 22:43:13 -0400

    Added function to output AlignObj for a list of peptides for run-pair

--- R/align_runs.R
+++ R/align_runs.R
@@ -351,7 +351,7 @@ getOswFiles <- function(peptides, filenames, dataPath = ".", query = NULL,
       dplyr::group_by(transition_group_id, peak_group_rank) %>%
       dplyr::mutate(transition_ids = paste0(transition_id, collapse = ","),
                     chromatogramIndex = paste0(chromatogramIndex, collapse = ",")) %>%
-      dplyr::ungroup() %>% dplyr::select(-transition_id, -peak_group_rank, -assay_RT, -delta_rt) %>% dplyr::distinct()
+      dplyr::ungroup() %>% dplyr::select(-transition_id, -assay_RT, -delta_rt) %>% dplyr::distinct()
     print(paste("Fetched chromatogram indices from run", filenames$runs[i]))
   }
   names(oswFiles) <- rownames(filenames)
@@ -393,6 +393,7 @@ getXICs <- function(peptides, runs, dataPath = ".", maxFdrQuery = 1.0,
   names(runs) <- rownames(filenames)
   # Get Chromatogram for each peptide in each run.
   XICs <- list()
+  print("Fetching Extracted-ion chromatograms from runs")
   for(i in 1:length(runs)){
     run <- names(runs)[i]
     mzmlName <- file.path(dataPath, "mzml", paste0(runs[run], ".chrom.mzML"))
@@ -415,7 +416,152 @@ getXICs <- function(peptides, runs, dataPath = ".", maxFdrQuery = 1.0,
     names(XICs_run) <- PeptidesFound
     XICs[[i]] <- XICs_run
     rm(mz)
+    print(paste("Fetched Extracted-ion chromatograms from run", runs[run]))
   }
   names(XICs) <- runs
   return(XICs)
+}
+
+
+#' AlignObj for peptides between a pair
+#'
+#' @return A list of AlignObj. Each AlignObj contains alignment path, similarity matrix and related parameters.
+#' @export
+getAlignObjs <- function(peptides, runs, dataPath = ".", alignType = "hybrid",
+                         query = NULL, oswMerged = TRUE, nameCutPattern = "(.*)(/)(.*)",
+                         maxFdrQuery = 0.05, maxFdrLoess = 0.01, spanvalue = 0.1,
+                         normalization = "mean", simMeasure = "dotProductMasked",
+                         SgolayFiltOrd = 4, SgolayFiltLen = 9,
+                         goFactor = 0.125, geFactor = 40,
+                         cosAngleThresh = 0.3, OverlapAlignment = TRUE,
+                         dotProdThresh = 0.96, gapQuantile = 0.5,
+                         hardConstrain = FALSE, samples4gradient = 100,
+                         expRSE = 8.0, samplingTime = 3.4,  RSEdistFactor = 3.5){
+  if(length(runs)!= 2){
+    print("For pairwise alignment, two runs are required.")
+    return(NULL)
+  }
+
+  if( (SgolayFiltLen %% 2) != 1){
+    print("SgolayFiltLen can only be odd number")
+    return(NULL)
+  }
+  # Check if names are consistent between osw and mzML files. Fetch run names.
+  filenames <- getRunNames(dataPath, oswMerged, nameCutPattern)
+  filenames <- filenames[filenames$runs %in% runs,]
+  rownames(filenames) <- paste0("run", 0:(length(runs)-1), "")
+
+  # Get Chromatogram indices for each peptide in each run.
+  oswFiles = getOswFiles(peptides, filenames, dataPath, query, oswMerged, maxFdrQuery, nameCutPattern)
+  PeptidesFound <- c()
+  for(x in oswFiles){
+    PeptidesFound <- x %>% .$transition_group_id %>% dplyr::union(PeptidesFound)
+  }
+
+  # Report peptides that are not found
+  PeptidesNotFound <- setdiff(peptides, PeptidesFound)
+  if(length(PeptidesNotFound)>0){
+    messsage(paste(PeptidesNotFound, "not found."))
+  }
+
+  ####################### Get XICs ##########################################
+  runs <- filenames$runs
+  names(runs) <- rownames(filenames)
+  # Get Chromatogram for each peptide in each run.
+  XICs <- list()
+  print("Fetching Extracted-ion chromatograms from runs")
+  for(i in 1:length(runs)){
+    run <- names(runs)[i]
+    mzmlName <- file.path(dataPath, "mzml", paste0(runs[run], ".chrom.mzML"))
+    mz <- tryCatch(mzR::openMSfile(mzmlName, backend = "pwiz"),
+                   error = function(cond) {
+                     c$message <- paste0(c$message,
+                                         "If error includes invalid cvParam accession 1002746, use FileConverter from OpenMS to decompress chromatograms")
+                     stop(cond)})
+    XICs_run <- lapply(1:length(PeptidesFound), function(j){
+      chromIndices <- oswFiles[[i]] %>%
+        dplyr::filter(transition_group_id == PeptidesFound[j]) %>% .$chromatogramIndex
+      if(length(chromIndices) > 0){
+        chromIndices <- as.integer(strsplit(chromIndices, split = ",")[[1]])
+        XIC_group <- extractXIC_group(mz, chromIndices, SgolayFiltOrd, SgolayFiltLen)
+      } else{
+        XIC_group <- list()
+      }
+      return(XIC_group)
+    })
+    names(XICs_run) <- PeptidesFound
+    XICs[[i]] <- XICs_run
+    rm(mz)
+    print(paste("Fetched Extracted-ion chromatograms from run", runs[run]))
+  }
+  names(XICs) <- runs
+
+  ####################### Perfrom alignment ##########################################
+  AlignObjs <- list()
+  loessFits <- list()
+  print("Perfroming alignment")
+  for(pepIdx in 1:length(peptides)){
+    peptide <- peptides[pepIdx]
+    # Select reference run based on m-score
+    minMscore <- 1; minrunIdx <- NA
+    for (runIdx in 1:length(oswFiles)){
+      m_score <- oswFiles[[runIdx]] %>%
+        dplyr::filter(transition_group_id == peptide & peak_group_rank == 1) %>% .$m_score
+      if(length(m_score) == 1){
+        if(m_score < minMscore){
+          minMscore <- m_score
+          minrunIdx <- runIdx
+        }
+      }
+    }
+    # Get XIC_group from reference run
+    ref <- names(runs)[minrunIdx]
+    exps <- setdiff(names(runs), ref)
+    XICs.ref <- XICs[[runs[ref]]][[peptide]]
+
+    # Align experiment run to reference run
+    for(eXp in exps){
+      # Get XIC_group from experiment run
+      XICs.eXp <- XICs[[runs[eXp]]][[peptide]]
+      if(length(XICs.eXp) > 0){
+        # Get the loess fit for hybrid alignment
+        pair <- paste(ref, eXp, sep = "_")
+        if(any(pair %in% names(loessFits))){
+          Loess.fit <- loessFits[[pair]]
+        } else{
+          df.ref <-  oswFiles[[ref]] %>% dplyr::filter(m_score <= maxFdrLoess & peak_group_rank == 1) %>%
+            dplyr::select(transition_group_id, RT)
+          df.eXp <-  oswFiles[[eXp]] %>% dplyr::filter(m_score <= maxFdrLoess & peak_group_rank == 1) %>%
+            dplyr::select(transition_group_id, RT)
+          RUNS_RT <- dplyr::inner_join(df.ref, df.eXp, by = "transition_group_id", suffix = c(".ref", ".eXp"))
+          Loess.fit <- loess(RT.eXp ~ RT.ref, data = RUNS_RT,
+                             span = spanvalue,
+                             control=loess.control(surface="direct"))
+          loessFits[[pair]] <- Loess.fit
+        }
+        # Set up constraints for penalizing similarity matrix
+        rse <- min(Loess.fit$s, expRSE)
+        noBeef <- ceiling(RSEdistFactor*rse/samplingTime)
+        tVec.ref <- XICs.ref[[1]][["time"]] # Extracting time component
+        tVec.eXp <- XICs.eXp[[1]][["time"]] # Extracting time component
+        B1p <- predict(Loess.fit, tVec.ref[1])
+        B2p <- predict(Loess.fit, tVec.ref[length(tVec.ref)])
+        # Perform dynamic programming for chromatogram alignment
+        intensityList.ref <- lapply(XICs.ref, `[[`, 2) # Extracting intensity values
+        intensityList.eXp <- lapply(XICs.eXp, `[[`, 2) # Extracting intensity values
+        Alignobj <- alignChromatogramsCpp(intensityList.ref, intensityList.eXp,
+                                          alignType = alignType, tVec.ref, tVec.eXp,
+                                          normalization = normalization, simType = simMeasure,
+                                          B1p = B1p, B2p = B2p, noBeef = noBeef,
+                                          goFactor = goFactor, geFactor = geFactor,
+                                          cosAngleThresh = cosAngleThresh, OverlapAlignment = OverlapAlignment,
+                                          dotProdThresh = dotProdThresh, gapQuantile = gapQuantile,
+                                          hardConstrain = hardConstrain, samples4gradient = samples4gradient)
+        AlignObjs[[pepIdx]] <- Alignobj
+      }
+      else {AlignObjs[[pepIdx]] <- NULL}
+    }
+  }
+  print("Alignment done. Returning AlignObjs")
+  return(AlignObjs)
 }
