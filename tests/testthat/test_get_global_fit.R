2730c01f651f4e8679605a2ae99f109ebf577373
shubham1637 <shubham.1637@gmail.com>
2019-11-24 20:42:10 -0500

    Added predict tests in global fit

--- tests/testthat/test_get_global_fit.R
+++ tests/testthat/test_get_global_fit.R
@@ -7,6 +7,8 @@ test_that("test_getLOESSfit", {
   # Testing for Residual standard error
   expect_equal(Loess.fit$s, 25.8761, tolerance = 1e-05)
   # Add predict function as well.
+  expect_equal(predict(Loess.fit, 4090.9), 4025.508, tolerance = 1e-05)
+  expect_equal(predict(Loess.fit, 4688.3), 4641.511, tolerance = 1e-05)
 })
 
 test_that("test_getLinearfit", {
@@ -15,5 +17,8 @@ test_that("test_getLinearfit", {
   Linear.fit <- getLinearfit(oswFiles, ref = "run2", eXp = "run0", maxFdrLoess = 0.05)
   # Testing for Residual standard error
   expect_equal(summary(Linear.fit)[["sigma"]], 26.58681, tolerance = 1e-05)
-  # Add predict function as well.
+  outData <- predict(Linear.fit, newdata = data.frame("RT.ref"=c(4090.9)))[[1]]
+  expect_equal(outData, 4027.519, tolerance = 1e-05)
+  outData <- predict(Linear.fit, newdata = data.frame("RT.ref"=c(4688.3)))[[1]]
+  expect_equal(outData, 4616.269, tolerance = 1e-05)
 })
