f9c78b84a3946321725c5c4458a27e631dfe9751
shubham1637 <shubham.1637@gmail.com>
2019-12-06 13:37:59 -0500

    Code for generating example.chrom.mzML files

--- tests/testthat/test_GenerateData.R
+++ tests/testthat/test_GenerateData.R
@@ -34,3 +34,25 @@ generateDIAlignRdata <- function(){
   XIC_KLYAGAILEV_2_DIAlignR <- outData
   save(XIC_KLYAGAILEV_2, file = "XIC_KLYAGAILEV_2_DIAlignR.RData")
 }
+
+generateDIAlignRchrom <- function(){
+  transition_group_id <- c("19051_KLIVTSEGC[160]FK/2", "19052_KLIVTSEGC[160]FK/3",
+  "1182_GLPIVNLLK/2", "997_SGEISLSSWEN/2", "13597_VVAGGELFKESVVVNDK/3", "4080_VITMPAGVELTNNNNVITVK/3",
+  "4081_VITM[147]PAGVELTNNNNVITVK/3", "12300_IHFLSPVRPFTLTPGDEEESFIQLITPVR/3", "14299_QFNNTDIVLLEDFQK/3",
+  "4731_GEANVELTPELAFK/2", "7351_ANAMGIPSLTVTNVPGSTLSR/3", "8704_HEQIDLFDSPNEGTR/2")
+  runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW", "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW",
+            "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW")
+  con <- DBI::dbConnect(RSQLite::SQLite(), dbname = paste0(runs[1], ".osw"))
+  # Generate a query.
+  query <- "SELECT PRECURSOR.GROUP_LABEL AS transition_group_id,
+  TRANSITION_PRECURSOR_MAPPING.TRANSITION_ID AS transition_id
+  FROM PRECURSOR
+  INNER JOIN TRANSITION_PRECURSOR_MAPPING ON TRANSITION_PRECURSOR_MAPPING.PRECURSOR_ID = PRECURSOR.ID
+  WHERE transition_group_id  = ?"
+  IDquery <- DBI::dbSendQuery(con, query)
+  DBI::dbBind(IDquery, list(transition_group_id))
+  NativeIDS <- DBI::dbFetch(IDquery)
+  DBI::dbClearResult(IDquery)
+  DBI::dbDisconnect(con)
+  write.table(NativeIDS, file = "NativeIDs.csv", sep = ",", row.names= FALSE)
+  }
