35e47bdb6dbfe1143f610502dc4726617029359c
shubham1637 <shubham.1637@gmail.com>
2019-12-07 17:20:08 -0500

    [Update] tests for example dataset

--- tests/testthat/test_GenerateData.R
+++ tests/testthat/test_GenerateData.R
@@ -1,38 +1,22 @@
 context("Generate test data")
 
 generateDIAlignRdata <- function(){
-  dataPath <- "../../data/testData2/"
-  oswMerged <- FALSE
+  dataPath <- "../../data/example/"
+  oswMerged <- TRUE
   nameCutPattern <- "(.*)(/)(.*)"
   filenames <- getRunNames(dataPath, oswMerged, nameCutPattern)
   oswFiles_DIAlignR <- getOswFiles(dataPath, filenames, maxFdrQuery = 0.05, analyteFDR = 0.01,
-                                   oswMerged, analytes = NULL, runType = "DIA_proteomics")
-  refAnalytes <- getAnalytesName(oswFiles_DIAlignR, 1.0, commonAnalytes = TRUE)
-  set.seed(1)
-  refAnalytes <- sample(refAnalytes, 400)
-  refAnalytes <- c(refAnalytes,"KLYAGAILEV_2")
-  for (i in seq_along(oswFiles_DIAlignR)){
-    keep <- which(oswFiles_DIAlignR[[i]]$transition_group_id %in% refAnalytes)
-    keep <- c(keep, sample(which(!oswFiles_DIAlignR[[i]]$transition_group_id %in% refAnalytes), 100))
-    oswFiles_DIAlignR[[i]] <- oswFiles_DIAlignR[[i]][keep,]
-  }
-  oswFiles_DIAlignR[[1]]$filename <- "DIAlignR/testData2/dia_files/191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms41.mzML"
-  oswFiles_DIAlignR[[2]]$filename <- "DIAlignR/testData2/dia_files/191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms42.mzML"
-  oswFiles_DIAlignR[[3]]$filename <- "DIAlignR/testData2/dia_files/191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms35.mzML"
+                                   oswMerged, analytes = NULL, runType = "DIA_proteomics", analyteInGroupLabel = TRUE)
   save(oswFiles_DIAlignR, file = "oswFiles_DIAlignR.RData")
 
-  analytes <- "KLYAGAILEV_2"
-  runs <- c("run0" = "191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms41",
-            "run1" = "191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms42",
-            "run2" = "191124_PM_BD-ZH12_shbh_10%_DIA_#1_400-650mz_msms35",
-            stringsAsFactors=FALSE)
-  runs <- c("run0" = "170407_AM_BD-ZH12_Spleen_W_10%_DIA_#1_400-650mz_msms41",
-            "run1" = "170407_AM_BD-ZH12_Spleen_W_10%_DIA_#2_400-650mz_msms42",
-            "run2" = "170413_AM_BD-ZH12_BoneMarrow_W_10%_DIA_#2_400-650mz_msms35")
+  analytes <- "14299_QFNNTDIVLLEDFQK/3"
+  runs <- c("run0" = "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt",
+            "run1" = "hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
+            "run2" = "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
   outData <- getXICs4AlignObj(dataPath, runs, oswFiles_DIAlignR, analytes,
-                              SgolayFiltOrd = 4, SgolayFiltLen = 5)
-  XIC_KLYAGAILEV_2_DIAlignR <- outData
-  save(XIC_KLYAGAILEV_2, file = "XIC_KLYAGAILEV_2_DIAlignR.RData")
+                              SgolayFiltOrd = 4, SgolayFiltLen = 13)
+  XIC_QFNNTDIVLLEDFQK_3_DIAlignR <- outData
+  save(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, file = "XIC_QFNNTDIVLLEDFQK_3_DIAlignR.RData")
 }
 
 generateDIAlignRchrom <- function(){
