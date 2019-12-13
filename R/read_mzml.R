1b3a15bdc0fe638d2158070c0768cf4f0f4089b8
shubham1637 <shubham.1637@gmail.com>
2019-12-13 14:02:39 -0500

    [Doc] documentation is done for read_mzml.R

--- R/read_mzml.R
+++ R/read_mzml.R
@@ -1,6 +1,21 @@
-#' Get a table of chromatogram indices and respective transition IDs.
+#' Get chromatogram header from a mzML file
 #'
-#' @return A data-frame.
+#' Get a table of chromatogram indices and respective transition IDs.
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + MIT
+#' Date: 2019-12-13
+#' @param mzmlName (char) path to mzml file.
+#' @return (A data-frame) It has 10 columns. The two important columns are:
+#' \item{chromatogramId}{(integer) Fragment-ion ID that matches with transition ID in osw file.}
+#' \item{chromatogramIndex}{(integer) Index of chromatogram in mzML file.}
+#'
+#' @examples
+#' dataPath <- system.file("extdata", package = "DIAlignR")
+#' mzmlName <- paste0(dataPath,"/mzml/hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt.chrom.mzML")
+#' chromHead <- readChromatogramHeader(mzmlName = mzmlName)
 readChromatogramHeader <- function(mzmlName){
   mz <- tryCatch(expr = mzR::openMSfile(mzmlName, backend = "pwiz"),
                  error = function(cnd) {
@@ -14,7 +29,22 @@ readChromatogramHeader <- function(mzmlName){
 
 #' Get pointers to each mzML file.
 #'
-#' @return A list.
+#' Returns instantiated mzRpwiz object associated to mzML file.
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + MIT
+#' Date: 2019-12-13
+#' @param dataPath (char) path to mzml and osw directory.
+#' @param runs (A vector of string) names of mzml file without extension. Vector must have names as shown in the example.
+#' @return (A list of mzRpwiz)
+#'
+#' @examples
+#' dataPath <- system.file("extdata", package = "DIAlignR")
+#' runs <- c("run0" = "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt", "run1" = "hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt")
+#' mzPntrs <- getMZMLpointers(dataPath = dataPath, runs = runs)
 getMZMLpointers <- function(dataPath, runs){
   mzPntrs <- list()
   for(mzMLindex in 1:length(runs)){
