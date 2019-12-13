f41e602c7eb2480aca042209fe8744306756c004
shubham1637 <shubham.1637@gmail.com>
2019-12-13 12:49:41 -0500

    [Doc] documentation is done for read_osw.R

--- R/read_osw.R
+++ R/read_osw.R
@@ -1,6 +1,43 @@
-#' Get a table of analytes, their chromatogram indices and scores.
+#' Fetch features of analytes
 #'
-#' @return A data-frame.
+#' Get a data-frame of analytes' transition_group_ids, their OpenSwath features, chromatogram indices and associated FDR-scores.
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + MIT
+#' Date: 2019-12-13
+#' @param oswName (char) path to the osw file.
+#' @param maxFdrQuery (numeric) A numeric value between 0 and 1. It is used to filter features from osw file which have SCORE_MS2.QVALUE less than itself.
+#' @param oswMerged (logical) TRUE for experiment-wide FDR and FALSE for run-specific FDR by pyprophet.
+#' @param analytes (vector of strings) transition_group_ids for which features are to be extracted. analyteInGroupLabel must be set according the pattern used here.
+#' @param filename (data-frame) Should be from the RUN.FILENAME column from osw files.
+#' @param runType (char) This must be one of the strings "DIA_proteomics", "DIA_Metabolomics".
+#' @param analyteInGroupLabel (logical) TRUE for getting analytes as PRECURSOR.GROUP_LABEL from osw file.
+#'  FALSE for fetching analytes as PEPTIDE.MODIFIED_SEQUENCE and PRECURSOR.CHARGE from osw file.
+#' @return (data-frames) Data-frame has following columns:
+#' \item{transition_group_id}{(string) it is either fetched from PRECURSOR.GROUP_LABEL or a combination of PEPTIDE.MODIFIED_SEQUENCE and PRECURSOR.CHARGE from osw file.}
+#' \item{filename}{(string) as mentioned in RUN table of osw files.}
+#' \item{RT}{(numeric) retention time as in FEATURE.EXP_RT of osw files.}
+#' \item{delta_rt}{(numeric) as in FEATURE.DELTA_RT of osw files.}
+#' \item{assay_RT}{(numeric) library retention time as in PRECURSOR.LIBRARY_RT of osw files.}
+#' \item{Intensity}{(numeric) peak intensity as in FEATURE_MS2.AREA_INTENSITY of osw files.}
+#' \item{leftWidth}{(numeric) as in FEATURE.LEFT_WIDTH of osw files.}
+#' \item{rightWidth}{(numeric) as in FEATURE.RIGHT_WIDTH of osw files.}
+#' \item{peak_group_rank}{(integer) rank of each feature associated with transition_group_id.}
+#' \item{m_score}{(numeric) q-value of each feature associated with transition_group_id.}
+#' \item{transition_id}{(integer) fragment-ion ID associated with transition_group_id. This is matched with chromatogram ID in mzML file.}
+#'
+#' @seealso \code{\link{getRunNames}}
+#' @examples
+#' dataPath <- system.file("extdata", package = "DIAlignR")
+#' filenames <- DIAlignR::getRunNames(dataPath = dataPath)
+#' oswName <- paste0(dataPath,"/osw/merged.osw")
+#' analytesInfo <- fetchAnalytesInfo(oswName, maxFdrQuery = 0.05, oswMerged = TRUE,
+#'  analytes = c("19051_KLIVTSEGC[160]FK/2"), filename = filenames$filename[2], runType = "DIA_proteomics", analyteInGroupLabel = TRUE)
+#' analytesInfo <- fetchAnalytesInfo(oswName, maxFdrQuery = 0.05, oswMerged = TRUE,
+#'  analytes = c("IHFLSPVRPFTLTPGDEEESFIQLITPVR_3"), filename = filenames$filename[3], runType = "DIA_proteomics", analyteInGroupLabel = FALSE)
 fetchAnalytesInfo <- function(oswName, maxFdrQuery, oswMerged,
                               analytes, filename, runType, analyteInGroupLabel = FALSE){
   # Establish a connection of SQLite file.
@@ -16,8 +53,7 @@ fetchAnalytesInfo <- function(oswName, maxFdrQuery, oswMerged,
 }
 
 
-
-#' Fetch analytes and from OSW file
+#' Fetch analytes from OSW file
 #'
 #' Get a data-frame of analytes, their chromatogram indices and associated FDR-scores.
 #'
@@ -46,9 +82,9 @@ fetchAnalytesInfo <- function(oswName, maxFdrQuery, oswMerged,
 #' @examples
 #' dataPath <- system.file("extdata", package = "DIAlignR")
 #' filenames <- DIAlignR::getRunNames(dataPath = dataPath)
-#' analytes <- getOswAnalytes(dataPath = dataPath, filenames = filenames, analyteInGroupLabel = TRUE)
+#' oswFiles <- getOswAnalytes(dataPath = dataPath, filenames = filenames, analyteInGroupLabel = TRUE)
 #' analytes[["run0"]][1,]
-#' analytes <- getOswAnalytes(dataPath = dataPath, filenames = filenames, analyteInGroupLabel = FALSE)
+#' oswFiles <- getOswAnalytes(dataPath = dataPath, filenames = filenames, analyteInGroupLabel = FALSE)
 #' analytes[["run0"]][1,]
 #' @export
 getOswAnalytes <- function(dataPath, filenames, oswMerged = TRUE, analyteInGroupLabel = FALSE,
