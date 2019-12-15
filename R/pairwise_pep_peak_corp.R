ec0540fa3491ccf76080f8c9e34ae40f138bcae7
shubham1637 <shubham.1637@gmail.com>
2019-12-14 22:30:38 -0500

    [Move] two functions from get_peaks_chromatograms.R to pairwise_pep_peak_corp,R

--- R/pairwise_pep_peak_corp.R
+++ R/pairwise_pep_peak_corp.R
@@ -158,3 +158,129 @@ getAlignedObj <- function(peptide, pairName, XICs.A=NULL, XICs.B=NULL, alignType
     return(Alignobj)
   }
 }
+
+
+
+
+#' Outputs AlignObj from an alignment of two XIC-groups
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + MIT
+#' Date: 2019-12-13
+#'
+#' @param XICs.ref List of extracted ion chromatograms from reference run.
+#' @param XICs.eXp List of extracted ion chromatograms from experiment run.
+#' @param Loess.fit LOESS fit object between reference and experiment run.
+#' @param adaptiveRT (numeric) Similarity matrix is not penalized within adaptive RT.
+#' @param samplingTime (numeric) Time difference between two data-points in each chromatogram. For hybrid and local alignment, samples are assumed to be equally time-spaced.
+#' @param normalization (character) Must be selected from "mean", "l2".
+#' @param simType (string) Must be selected from dotProduct, cosineAngle,
+#' cosine2Angle, dotProductMasked, euclideanDist, covariance and correlation.
+#' @param goFactor (numeric) Penalty for introducing first gap in alignment. This value is multiplied by base gap-penalty.
+#' @param geFactor (numeric) Penalty for introducing subsequent gaps in alignment. This value is multiplied by base gap-penalty.
+#' @param cosAngleThresh (numeric) In simType = dotProductMasked mode, angular similarity should be higher than cosAngleThresh otherwise similarity is forced to zero.
+#' @param OverlapAlignment (logical) An input for alignment with free end-gaps. False: Global alignment, True: overlap alignment.
+#' @param dotProdThresh (numeric) In simType = dotProductMasked mode, values in similarity matrix higher than dotProdThresh quantile are checked for angular similarity.
+#' @param gapQuantile (numeric) Must be between 0 and 1. This is used to calculate base gap-penalty from similarity distribution.
+#' @param hardConstrain (logical) If FALSE; indices farther from noBeef distance are filled with distance from linear fit line.
+#' @param samples4gradient (numeric) This parameter modulates penalization of masked indices.
+#' @param objType (char) Must be selected from light, medium and heavy.
+#' @return A S4 object. Three most-important slots are:
+#' \item{indexA_aligned}{(integer) aligned indices of reference run.}
+#' \item{indexB_aligned}{(integer) aligned indices of experiment run.}
+#' \item{score}{(numeric) cumulative score of alignment.}
+#'
+#' @seealso \code{\link{alignChromatogramsCpp}}
+#' @examples
+#' data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
+#' data(oswFiles_DIAlignR, package="DIAlignR")
+#' XICs.ref <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]]
+#' XICs.eXp <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]]
+#' Loess.fit <- getGlobalAlignment(oswFiles_DIAlignR, ref = "run1", eXp = "run2", maxFdrGlobal = 0.05, spanvalue = 0.1)
+#' AlignObj <- getAlignObj(XICs.ref, XICs.eXp, Loess.fit, adaptiveRT = 77.82315, samplingTime = 3.414,
+#' normalization = "mean", simType = "dotProductMasked", goFactor = 0.125, geFactor = 40,
+#' cosAngleThresh = 0.3, OverlapAlignment = TRUE, dotProdThresh = 0.96, gapQuantile = 0.5, hardConstrain = FALSE,
+#' samples4gradient = 100, objType = "light")
+getAlignObj <- function(XICs.ref, XICs.eXp, Loess.fit, adaptiveRT, samplingTime,
+                        normalization, simType, goFactor, geFactor,
+                        cosAngleThresh, OverlapAlignment,
+                        dotProdThresh, gapQuantile, hardConstrain,
+                        samples4gradient, objType = "light"){
+  # Set up constraints for penalizing similarity matrix
+  noBeef <- ceiling(adaptiveRT/samplingTime)
+  tVec.ref <- XICs.ref[[1]][["time"]] # Extracting time component
+  tVec.eXp <- XICs.eXp[[1]][["time"]] # Extracting time component
+  B1p <- predict(Loess.fit, tVec.ref[1])
+  B2p <- predict(Loess.fit, tVec.ref[length(tVec.ref)])
+  # Perform dynamic programming for chromatogram alignment
+  intensityList.ref <- lapply(XICs.ref, `[[`, 2) # Extracting intensity values
+  intensityList.eXp <- lapply(XICs.eXp, `[[`, 2) # Extracting intensity values
+  AlignObj <- alignChromatogramsCpp(intensityList.ref, intensityList.eXp,
+                                    alignType = "hybrid", tVec.ref, tVec.eXp,
+                                    normalization = normalization, simType = simType,
+                                    B1p = B1p, B2p = B2p, noBeef = noBeef,
+                                    goFactor = goFactor, geFactor = geFactor,
+                                    cosAngleThresh = cosAngleThresh, OverlapAlignment = OverlapAlignment,
+                                    dotProdThresh = dotProdThresh, gapQuantile = gapQuantile,
+                                    hardConstrain = hardConstrain, samples4gradient = samples4gradient,
+                                    objType = objType)
+  AlignObj
+}
+
+#' Get mapping of reference RT on experiment run.
+#'
+#' This function aligns XICs of reference and experiment runs. Using alignment, it maps retention time from refernce run on experiment run.
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + MIT
+#' Date: 2019-12-13
+#' @param XICs.ref List of extracted ion chromatograms from reference run.
+#' @param XICs.eXp List of extracted ion chromatograms from experiment run.
+#' @param Loess.fit LOESS fit object between reference and experiment run.
+#' @param alignType Available alignment methods are "global", "local" and "hybrid".
+#' @param adaptiveRT (numeric) Similarity matrix is not penalized within adaptive RT.
+#' @param samplingTime (numeric) Time difference between two data-points in each chromatogram. For hybrid and local alignment, samples are assumed to be equally time-spaced.
+#' @param normalization (character) Must be selected from "mean", "l2".
+#' @param simMeasure (string) Must be selected from dotProduct, cosineAngle,
+#' cosine2Angle, dotProductMasked, euclideanDist, covariance and correlation.
+#' @param goFactor (numeric) Penalty for introducing first gap in alignment. This value is multiplied by base gap-penalty.
+#' @param geFactor (numeric) Penalty for introducing subsequent gaps in alignment. This value is multiplied by base gap-penalty.
+#' @param cosAngleThresh (numeric) In simType = dotProductMasked mode, angular similarity should be higher than cosAngleThresh otherwise similarity is forced to zero.
+#' @param OverlapAlignment (logical) An input for alignment with free end-gaps. False: Global alignment, True: overlap alignment.
+#' @param dotProdThresh (numeric) In simType = dotProductMasked mode, values in similarity matrix higher than dotProdThresh quantile are checked for angular similarity.
+#' @param gapQuantile (numeric) Must be between 0 and 1. This is used to calculate base gap-penalty from similarity distribution.
+#' @param hardConstrain (logical) If FALSE; indices farther from noBeef distance are filled with distance from linear fit line.
+#' @param samples4gradient (numeric) This parameter modulates penalization of masked indices.
+#' @param objType (char) Must be selected from light, medium and heavy.
+#' @return (numeric)
+#' @seealso \code{\link{alignChromatogramsCpp}}
+#' @examples
+#' data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
+#' data(oswFiles_DIAlignR, package="DIAlignR")
+#' XICs.ref <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]]
+#' XICs.eXp <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]]
+#' Loess.fit <- getGlobalAlignment(oswFiles_DIAlignR, ref = "run2", eXp = "run0", maxFdrGlobal = 0.05, spanvalue = 0.1)
+#' adaptiveRT <- 77.82315 #3.5*Loess.fit$s
+#' getMappedRT(refRT = 5238.35, XICs.ref, XICs.eXp, Loess.fit, alignType = "hybrid",
+#'  adaptiveRT = adaptiveRT, samplingTime = 3.414, normalization = "mean", simMeasure = "dotProductMasked",
+#'  goFactor = 0.125, geFactor = 40, cosAngleThresh = 0.3, OverlapAlignment = TRUE,
+#'  dotProdThresh = 0.96, gapQuantile = 0.5, hardConstrain = FALSE, samples4gradient = 100)
+#' @export
+getMappedRT <- function(refRT, XICs.ref, XICs.eXp, Loess.fit, alignType, adaptiveRT, samplingTime,
+                        normalization, simMeasure, goFactor, geFactor, cosAngleThresh,
+                        OverlapAlignment, dotProdThresh, gapQuantile, hardConstrain,
+                        samples4gradient, objType = "light"){
+  AlignObj <- getAlignObj(XICs.ref, XICs.eXp, Loess.fit, adaptiveRT, samplingTime,
+                          normalization, simType = simMeasure, goFactor, geFactor,
+                          cosAngleThresh, OverlapAlignment,
+                          dotProdThresh, gapQuantile, hardConstrain, samples4gradient, objType)
+  tVec.ref <- XICs.ref[[1]][["time"]] # Extracting time component
+  tVec.eXp <- XICs.eXp[[1]][["time"]] # Extracting time component
+  eXpRT <- mappedRTfromAlignObj(refRT, tVec.ref, tVec.eXp, AlignObj)
+  eXpRT
+}
--- tests/testthat/test_get_peaks_chromatograms.R
+++ tests/testthat/test_get_peaks_chromatograms.R
@@ -44,42 +44,6 @@ test_that("test_getXICs4AlignObj", {
   expect_equal(outData[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
 })
 
-test_that("test_getAlignObj", {
-  data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
-  XICs <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR
-  data(oswFiles_DIAlignR, package="DIAlignR")
-  oswFiles <- oswFiles_DIAlignR
-  XICs.ref <- XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]]
-  XICs.eXp <- XICs[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]]
-  Loess.fit <- getLOESSfit(oswFiles, ref = "run1", eXp = "run2", maxFdrGlobal = 0.05, spanvalue = 0.1)
-  outData <- getAlignObj(XICs.ref, XICs.eXp, Loess.fit, adaptiveRT = 77.82315, samplingTime = 3.414,
-              normalization = "mean", simType = "dotProductMasked", goFactor = 0.125, geFactor = 40,
-              cosAngleThresh = 0.3, OverlapAlignment = TRUE,
-              dotProdThresh = 0.96, gapQuantile = 0.5, hardConstrain = FALSE,
-              samples4gradient = 100, objType = "light")
-  expData <- testAlignObj()
-  expect_equal(outData, expData, tolerance = 1e-03)
-})
-
-test_that("test_getMappedRT", {
-  data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
-  XICs <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR
-  data(oswFiles_DIAlignR, package="DIAlignR")
-  oswFiles <- oswFiles_DIAlignR
-  XICs.ref <- XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]]
-  XICs.eXp <- XICs[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]]
-  adaptiveRT <- 77.82315 #3.5*Loess.fit$s
-  Loess.fit <- getLOESSfit(oswFiles, ref = "run2", eXp = "run0", maxFdrGlobal = 0.05, spanvalue = 0.1)
-  outData <- getMappedRT(refRT = 5238.35, XICs.ref, XICs.eXp, Loess.fit, alignType = "hybrid",
-                         adaptiveRT = adaptiveRT, samplingTime = 3.414,
-                         normalization = "mean", simMeasure = "dotProductMasked",
-                         goFactor = 0.125, geFactor = 40, cosAngleThresh = 0.3,
-                         OverlapAlignment = TRUE,
-                         dotProdThresh = 0.96, gapQuantile = 0.5, hardConstrain = FALSE,
-                         samples4gradient = 100)
-  expect_equal(outData, 5237.8, tolerance = 1e-03)
-})
-
 test_that("test_getXICs", {
   runs <- c("hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt", "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
   outData <- getXICs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = "../../inst/extdata",
