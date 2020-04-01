7ac2c55c6ebc3ddee5fbd17c5eea3c09eaafc013
shubham1637 <shubham.1637@gmail.com>
2020-03-31 22:23:40 -0400

    [as.list] Added as.list for S4 objects

--- R/affine_aligned_obj.R
+++ R/affine_aligned_obj.R
@@ -1,3 +1,82 @@
+# showMethods(as.list)
+# methods("as.list")
+# We have to use variable x because that is what used in generic.
+
+#' An S4 object for class AffineAlignObjLight It only contains aligned indices.
+#'
+#' indexA_aligned and indexB_aligned are aligned indices of signalA and SignalB. The cumulative
+#' alignment score is in score vector.
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + GPL-3
+#' Date: 2019-12-14
+#' @importFrom methods setClass new
+#' @seealso \code{\link{doAffineAlignmentCpp}}
+#' @export
+AffineAlignObjLight <- setClass(Class="AffineAlignObjLight",
+                                representation(indexA_aligned = "numeric", indexB_aligned = "numeric",
+                                               score = "numeric")
+)
+
+#' Converts instances of class AffineAlignObjLight into list
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + GPL-3
+#' Date: 2020-03-31
+#' @importFrom methods setMethod slot slotNames
+#' @param x An object of class AffineAlignObjLight
+#' @export
+setMethod("as.list", signature(x="AffineAlignObjLight"), function(x) {
+  mapply(function(slotname) slot(x, slotname), slotNames(class(x)), SIMPLIFY=FALSE)
+})
+
+
+
+
+#' An S4 object for class AffineAlignObjMedium. It only contains similarity matrix and aligned indices.
+#'
+#' s is a point-wise similarity matrix between signalA and signalB.
+#' path matrix is a binary matrix with ones indicating path of maximum cumulative score.
+#' GapOpen and GapExten are gap-opening and gap-extension penalties used by affine alignment algorithm.
+#' indexA_aligned and indexB_aligned are aligned indices of signalA and SignalB. The cumulative
+#' alignment score is in score vector.
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + GPL-3
+#' Date: 2019-12-14
+#' @importFrom methods setClass new
+#' @seealso \code{\link{doAffineAlignmentCpp}}
+#' @export
+AffineAlignObjMedium <- setClass(Class="AffineAlignObjMedium",
+                        representation(s = "matrix", path = "matrix"),
+                        contains = "AffineAlignObjLight"
+)
+
+#' Converts instances of class AffineAlignObjMedium into list
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + GPL-3
+#' Date: 2020-03-31
+#' @importFrom methods setMethod slot slotNames
+#' @param x An object of class AffineAlignObjMedium
+#' @export
+setMethod("as.list", signature(x="AffineAlignObjMedium"), function(x) {
+  mapply(function(slotname) slot(x, slotname), slotNames(class(x)), SIMPLIFY=FALSE)
+  })
+
+
+
+
 #' An S4 object for class AffineAlignObj
 #'
 #' s is a point-wise similarity matrix between signalA and signalB.
@@ -17,56 +96,33 @@
 #' @seealso \code{\link{doAffineAlignmentCpp}}
 #' @export
 AffineAlignObj <- setClass(Class="AffineAlignObj",
-                   representation(s = "matrix", M = "matrix", A = "matrix", B = "matrix",
-                                  Traceback = "matrix",
-                                  path = "matrix",
-                                  signalA_len = "numeric", signalB_len = "numeric",
-                                  GapOpen = "numeric", GapExten = "numeric",
-                                  FreeEndGaps = "logical",
-                                  indexA_aligned = "numeric", indexB_aligned = "numeric",
-                                  score = "numeric", simScore_forw = "numeric",
-                                  nGaps = "numeric")
-                   )
+                           representation(s = "matrix", M = "matrix", A = "matrix", B = "matrix",
+                                          Traceback = "matrix",
+                                          path = "matrix",
+                                          signalA_len = "numeric", signalB_len = "numeric",
+                                          GapOpen = "numeric", GapExten = "numeric",
+                                          FreeEndGaps = "logical",
+                                          indexA_aligned = "numeric", indexB_aligned = "numeric",
+                                          score = "numeric", simScore_forw = "numeric",
+                                          nGaps = "numeric")
+)
 
-#' An S4 object for class AffineAlignObj. It only contains aligned indices.
+#' Converts instances of class AffineAlignObj into list
 #'
-#' indexA_aligned and indexB_aligned are aligned indices of signalA and SignalB. The cumulative
-#' alignment score is in score vector.
 #' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
 #'
 #' ORCID: 0000-0003-3500-8152
 #'
 #' License: (c) Author (2019) + GPL-3
-#' Date: 2019-12-14
-#' @importFrom methods setClass new
-#' @seealso \code{\link{doAffineAlignmentCpp}}
+#' Date: 2020-03-31
+#' @importFrom methods setMethod slot slotNames
+#' @param x An object of class AffineAlignObj.
 #' @export
-AffineAlignObjLight <- setClass(Class="AffineAlignObjLight",
-                           representation(indexA_aligned = "numeric", indexB_aligned = "numeric",
-                                          score = "numeric")
-)
+setMethod("as.list", signature(x="AffineAlignObj"), function(x) {
+  mapply(function(slotname) slot(x, slotname), slotNames(class(x)), SIMPLIFY=FALSE)
+})
 
-#' An S4 object for class AffineAlignObj. It only contains similarity matrix and aligned indices.
-#'
-#' s is a point-wise similarity matrix between signalA and signalB.
-#' path matrix is a binary matrix with ones indicating path of maximum cumulative score.
-#' GapOpen and GapExten are gap-opening and gap-extension penalties used by affine alignment algorithm.
-#' indexA_aligned and indexB_aligned are aligned indices of signalA and SignalB. The cumulative
-#' alignment score is in score vector.
-#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
-#'
-#' ORCID: 0000-0003-3500-8152
-#'
-#' License: (c) Author (2019) + GPL-3
-#' Date: 2019-12-14
-#' @importFrom methods setClass new
-#' @seealso \code{\link{doAffineAlignmentCpp}}
-#' @export
-AffineAlignObjMedium <- setClass(Class="AffineAlignObjMedium",
-                        representation(s = "matrix", path = "matrix",
-                                       indexA_aligned = "numeric", indexB_aligned = "numeric",
-                                               score = "numeric")
-)
+
 
 
 #' An S4 object for class AlignObj
@@ -88,13 +144,28 @@ AffineAlignObjMedium <- setClass(Class="AffineAlignObjMedium",
 #' @seealso \code{\link{doAlignmentCpp}}
 #' @export
 AlignObj <- setClass(Class="AlignObj",
-                     representation(s = "matrix", M = "matrix", Traceback = "matrix",
-                                    path = "matrix", optionalPaths = "matrix",
+                     representation(M = "matrix", Traceback = "matrix",
+                                    optionalPaths = "matrix",
                                     M_forw = "matrix",
                                     signalA_len = "numeric", signalB_len = "numeric",
                                     GapOpen = "numeric", GapExten = "numeric",
                                     FreeEndGaps = "logical",
-                                    indexA_aligned = "numeric", indexB_aligned = "numeric",
-                                    score = "numeric", score_forw = "numeric", simScore_forw = "numeric",
-                                    nGaps = "numeric")
+                                    score_forw = "numeric", simScore_forw = "numeric",
+                                    nGaps = "numeric"),
+                     contains = "AffineAlignObjMedium"
 )
+
+#' Converts instances of class AlignObj into list
+#'
+#' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
+#'
+#' ORCID: 0000-0003-3500-8152
+#'
+#' License: (c) Author (2019) + GPL-3
+#' Date: 2020-03-31
+#' @importFrom methods setMethod slot slotNames
+#' @param x An object of class AlignObj
+#' @export
+setMethod("as.list", signature(x="AlignObj"), function(x) {
+  mapply(function(slotname) slot(x, slotname), slotNames(class(x)), SIMPLIFY=FALSE)
+})
--- vignettes/DIAlignR-vignette.R
+++ vignettes/DIAlignR-vignette.R
@@ -30,11 +30,11 @@ intensityTbl <- alignTargetedRuns(dataPath = dataPath, runs = NULL, analytes = N
 ## ---- message=FALSE--------------------------------------------------------
 runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
           "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-AlignObjLight <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath,
-                              objType	= "light")
-slotNames(AlignObjLight[["QFNNTDIVLLEDFQK_3"]][[1]])
-AlignObjMedium <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath,
-                               objType	= "medium")
+AlignObjLight <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath, objType	= "light")
+obj <- AlignObjLight[["QFNNTDIVLLEDFQK_3"]][[1]]
+slotNames(obj)
+names(as.list(obj))
+AlignObjMedium <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath, objType	= "medium")
 slotNames(AlignObjMedium[["QFNNTDIVLLEDFQK_3"]][[1]])
 
 ## ---- fig.width=6, fig.align='center', fig.height=6, message=FALSE---------
