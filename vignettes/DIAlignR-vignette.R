7d0c18815eff584550b718f34b6cb1910a5bddef
shubham1637 <shubham.1637@gmail.com>
2020-04-17 20:52:40 -0400

    Updated vignette and namespace

--- vignettes/DIAlignR-vignette.R
+++ vignettes/DIAlignR-vignette.R
@@ -19,35 +19,34 @@ dataPath <- system.file("extdata", package = "DIAlignR")
 runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
           "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
 # For specific runs provide their names.
-intensityTbl <- alignTargetedRuns(dataPath = dataPath, runs = runs, analyteInGroupLabel = TRUE)
-# For specific analytes provide their names.
-intensityTbl <- alignTargetedRuns(dataPath = dataPath, runs = NULL,
-                                  analytes = c("QFNNTDIVLLEDFQK_3"), analyteInGroupLabel = FALSE)
+alignTargetedRuns(dataPath = dataPath, outFile = "test.csv", runs = runs, oswMerged = TRUE)
 # For all the analytes in all runs, keep them as NULL.
-intensityTbl <- alignTargetedRuns(dataPath = dataPath, runs = NULL, analytes = NULL,
-                                  analyteInGroupLabel = TRUE)
+alignTargetedRuns(dataPath = dataPath, outFile = "test.csv", runs = NULL, oswMerged = TRUE)
 
 ## ---- message=FALSE--------------------------------------------------------
 runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
           "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-AlignObjLight <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath, objType	= "light")
-obj <- AlignObjLight[["QFNNTDIVLLEDFQK_3"]][[1]]
+AlignObjLight <- getAlignObjs(analytes = 4618L, runs = runs, dataPath = dataPath, objType	= "light")
+# First element contains names of runs, spectra files, chromatogram files and feature files.
+AlignObjLight[[1]][, c("runName", "spectraFile")]
+obj <- AlignObjLight[[2]][["4618"]][[1]][["AlignObj"]]
 slotNames(obj)
 names(as.list(obj))
-AlignObjMedium <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath, objType	= "medium")
-slotNames(AlignObjMedium[["QFNNTDIVLLEDFQK_3"]][[1]])
+AlignObjMedium <- getAlignObjs(analytes = 4618L, runs = runs, dataPath = dataPath, objType	= "medium")
+obj <- AlignObjMedium[[2]][["4618"]][[1]][["AlignObj"]]
+slotNames(obj)
 
 ## ---- fig.width=6, fig.align='center', fig.height=6, message=FALSE---------
 runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
  "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-AlignObj <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath)
+AlignObj <- getAlignObjs(analytes = 4618L, runs = runs, dataPath = dataPath)
 plotAlignedAnalytes(AlignObj, annotatePeak = TRUE)
 
 ## ---- fig.width=5, fig.align='center', fig.height=5, message=FALSE---------
 library(lattice)
 runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
  "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-AlignObjOutput <- getAlignObjs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs,
+AlignObjOutput <- getAlignObjs(analytes = 4618L, runs = runs,
                                dataPath = dataPath, objType = "medium")
 plotAlignmentPath(AlignObjOutput)
 
