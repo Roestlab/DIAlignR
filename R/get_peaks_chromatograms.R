7e2c969b6f117efc6d0fb158cdb1447b6ca1e3d2
shubham1637 <shubham.1637@gmail.com>
2020-04-17 20:36:35 -0400

    [Fix] extracting XICs for specific analyte and run. Updated documentation

--- R/get_peaks_chromatograms.R
+++ R/get_peaks_chromatograms.R
@@ -1,6 +1,6 @@
-#' Extract XICs of all transitions requested in chromIndices.
+#' Extract XICs of chromIndices
 #'
-#' Extracts XICs using mz object. Generally Savitzky–Golay filter is used, however, filter can be turned-off as well.
+#' Extracts XICs using mz object. Each chromatogram represents a transition of precursor.
 #' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
 #'
 #' ORCID: 0000-0003-3500-8152
@@ -24,10 +24,10 @@ extractXIC_group <- function(mz, chromIndices){
   XIC_group
 }
 
-#' Extract XICs of all analytes from oswFiles
+#' Extract XICs of analytes
 #'
-#' For all the analytes requested, it fetches chromatogram indices from oswFiles and
-#' extract chromatograms from mzML files.
+#' For all the analytes requested, it fetches chromatogram indices from prec2chromIndex and
+#' extracts chromatograms using mzPntrs.
 #'
 #' @author Shubham Gupta, \email{shubh.gupta@mail.utoronto.ca}
 #'
@@ -35,41 +35,47 @@ extractXIC_group <- function(mz, chromIndices){
 #'
 #' License: (c) Author (2019) + GPL-3
 #' Date: 2019-12-13
-#' @param dataPath (char) path to mzml and osw directory.
-#' @param runs (vector of string) names of mzML files without extension. Names of the vector must be a combination of "run" and an iteger e.g. "run2".
-#' @param oswFiles (list of data-frames) it is output from getOswFiles function.
-#' @param analytes (string) analyte is as PRECURSOR.GROUP_LABEL or as PEPTIDE.MODIFIED_SEQUENCE and PRECURSOR.CHARGE from osw file.
-#' @param XICfilter (string) this must be one of the strings "sgolay", "none".
-#' @param SgolayFiltOrd (integer) it defines the polynomial order of filer.
-#' @param SgolayFiltLen (integer) must be an odd number. It defines the length of filter.
-#' @param mzPntrs A list of mzRpwiz.
+#' @param mzPntrs a list of mzRpwiz.
+#' @param fileInfo (data-frame) output of getRunNames().
+#' @param runs (vector of string) names of mzML files without extension.
+#' @param prec2chromIndex (list of data-frames) output of getChromatogramIndices(). Each dataframe has two columns: transition_group_id and chromatogramIndex.
+#' @param analytes (integer) a vector of precursor IDs.
 #' @return A list of list of data-frames. Each data frame has elution time and intensity of fragment-ion XIC.
 #'
-#' @seealso \code{\link{getOswFiles}, \link{getRunNames}}
+#' @seealso \code{\link{getChromatogramIndices}, \link{getRunNames}}
 #' @examples
 #' dataPath <- system.file("extdata", package = "DIAlignR")
-#' filenames <- DIAlignR::getRunNames(dataPath = dataPath)
-#' runs <- c("run1" = "hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
-#'           "run0" =  "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt")
-#' oswFiles <- DIAlignR::getOswFiles(dataPath, filenames)
-#' analytes <- "QFNNTDIVLLEDFQK_3"
-#' XICs <- getXICs4AlignObj(dataPath, runs, oswFiles, analytes)
+#' runs <- c("hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
+#'           "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt")
+#' analytes <- c(32L, 898L, 2474L)
+#' fileInfo <- getRunNames(dataPath = dataPath)
+#' fileInfo <- updateFileInfo(fileInfo, runs)
+#' precursors <- getPrecursorByID(analytes,fileInfo)
+#' mzPntrs <- getMZMLpointers(fileInfo)
+#' prec2chromIndex <- getChromatogramIndices(fileInfo, precursors, mzPntrs)
+#' XICs <- getXICs4AlignObj(mzPntrs, fileInfo, runs, prec2chromIndex, analytes)
+#' rm(mzPntrs)
 #' @export
-getXICs4AlignObj <- function(dataPath, runs, oswFiles, analytes, XICfilter = "sgolay",
-                             SgolayFiltOrd = 4, SgolayFiltLen = 9, mzPntrs = NULL){
-  if(is.null(mzPntrs)){
-    mzPntrs <- getMZMLpointers(dataPath, runs)
-  }
-  XICs <- vector("list", length(runs))
-  names(XICs) <- names(runs)
+getXICs4AlignObj <- function(mzPntrs, fileInfo, runs, prec2chromIndex, analytes){
+  # Select ony runs that are present in fileInfo.
+  fileInfo <- updateFileInfo(fileInfo, runs)
+  runs <- rownames(fileInfo)
+  # Create empty list to store chromatograms.
+  XICs <- vector("list", nrow(fileInfo))
+  names(XICs) <- fileInfo$runName
   for(i in seq_along(runs)){
-    runname = names(runs)[i]
-    message("Fetching XICs from run ", runs[[runname]])
+    runname = runs[i]
+    # Get precursor to chromatogram Indices mapping.
+    chromMapping <- prec2chromIndex[[runname]]
+    message("Fetching XICs from run ", fileInfo$chromatogramFile[i])
+    # From each run iterate over analytes.
     XICs[[i]] <- lapply(seq_along(analytes), function(j){
       analyte <- analytes[j]
-      chromIndices <- selectChromIndices(oswFiles, runname = runname, analyte = analyte)
-      if(is.null(chromIndices)){
-        warning("Chromatogram indices for ", analyte, " are missing in ", runs[[runname]])
+      idx <- which(chromMapping[["transition_group_id"]] == analyte)
+      chromIndices <- chromMapping[["chromatogramIndex"]][[idx]]
+      # If chromatogram indices are missing, add NULL as XICs for the analyte.
+      if(any(is.na(chromIndices))){
+        warning("Chromatogram indices for ", analyte, " are missing in ", fileInfo$chromatogramFile[i])
         message("Skipping ", analyte)
         XIC_group <- NULL
       } else {
@@ -77,9 +83,9 @@ getXICs4AlignObj <- function(dataPath, runs, oswFiles, analytes, XICfilter = "sg
       }
       XIC_group
     })
-    names(XICs[[i]]) <- analytes
+    # Name the XICs as analyte(coerced as character).
+    names(XICs[[i]]) <- as.character(analytes)
   }
-  rm(mzPntrs)
   XICs
 }
 
@@ -96,55 +102,46 @@ getXICs4AlignObj <- function(dataPath, runs, oswFiles, analytes, XICfilter = "sg
 #' License: (c) Author (2019) + GPL-3
 #' Date: 2019-12-13
 #'
-#' @param analytes (string) An analyte is as PRECURSOR.GROUP_LABEL or as PEPTIDE.MODIFIED_SEQUENCE and PRECURSOR.CHARGE from osw file.
-#' @param runs (A vector of string) Names of mzml file without extension. Vector must have names as shown in the example.
-#' @param dataPath (char) Path to mzml and osw directory.
+#' @param analytes (integer) a vector of precursor IDs.
+#' @param runs (vector of string) names of mzML files without extension.
+#' @param dataPath (string) Path to mzml and osw directory.
 #' @param maxFdrQuery (numeric) A numeric value between 0 and 1. It is used to filter features from osw file which have SCORE_MS2.QVALUE less than itself.
-#' @param XICfilter (string) This must be one of the strings "sgolay", "none".
-#' @param SgolayFiltOrd (integer) It defines the polynomial order of filer.
-#' @param SgolayFiltLen (integer) Must be an odd number. It defines the length of filter.
 #' @param runType (char) This must be one of the strings "DIA_proteomics", "DIA_Metabolomics".
 #' @param oswMerged (logical) TRUE for experiment-wide FDR and FALSE for run-specific FDR by pyprophet.
-#' @param nameCutPattern (string) regex expression to fetch mzML file name from RUN.FILENAME columns of osw files.
-#' @param analyteInGroupLabel (logical) TRUE for getting analytes as PRECURSOR.GROUP_LABEL from osw file.
 #' @return A list of list. Each list contains XIC-group for that run. XIC-group is a list of dataframe that has elution time and intensity of fragment-ion XIC.
 #'
-#' @seealso \code{\link{getOswFiles}, \link{getRunNames}}
+#' @seealso \code{\link{getXICs4AlignObj}, \link{getRunNames}, \link{analytesFromFeatures}}
 #' @examples
 #' dataPath <- system.file("extdata", package = "DIAlignR")
 #' runs <- c("hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt",
 #'  "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-#' XICs <- getXICs(analytes = c("QFNNTDIVLLEDFQK_3"), runs = runs, dataPath = dataPath)
+#' analytes <- c(32L, 898L, 2474L)
+#' XICs <- getXICs(analytes, runs = runs, dataPath = dataPath)
 #' @export
-getXICs <- function(analytes, runs, dataPath = ".", maxFdrQuery = 1.0, XICfilter = "sgolay",
-                    SgolayFiltOrd = 4, SgolayFiltLen = 9, runType = "DIA_proteomics",
-                    oswMerged = TRUE, nameCutPattern = "(.*)(/)(.*)", analyteInGroupLabel = FALSE){
-  if( (SgolayFiltLen %% 2) != 1){
-    print("SgolayFiltLen can only be odd number")
-    return(NULL)
-  }
-  # Get filenames from .merged.osw file and check if names are consistent between osw and mzML files.
-  filenames <- getRunNames(dataPath, oswMerged, nameCutPattern)
-  filenames <- filenames[filenames$runs %in% runs,]
+getXICs <- function(analytes, runs, dataPath = ".", maxFdrQuery = 1.0, runType = "DIA_proteomics",
+                    oswMerged = TRUE){
+  # Get fileInfo from .merged.osw file and check if names are consistent between osw and mzML files.
+  fileInfo <- getRunNames(dataPath, oswMerged)
+  fileInfo <- updateFileInfo(fileInfo, runs)
+
+  precursors <- getPrecursorByID(analytes,fileInfo)
+  mzPntrs <- getMZMLpointers(fileInfo)
+  prec2chromIndex <- getChromatogramIndices(fileInfo, precursors, mzPntrs)
 
   # Get Chromatogram indices for each peptide in each run.
-  oswFiles <- getOswFiles(dataPath, filenames, maxFdrQuery = maxFdrQuery, analyteFDR = 1.00,
-                         oswMerged = oswMerged, analytes = analytes, runType = runType,
-                         analyteInGroupLabel = analyteInGroupLabel)
-  refAnalytes <- getAnalytesName(oswFiles, commonAnalytes = FALSE)
+  features <- getFeatures(fileInfo, maxFdrQuery, runType)
+  refAnalytes <-  analytesFromFeatures(features, analyteFDR = maxFdrQuery, commonAnalytes = FALSE)
   analytesFound <- intersect(analytes, refAnalytes)
   analytesNotFound <- setdiff(analytes, analytesFound)
   if(length(analytesNotFound)>0){
-    message("Analytes ", paste(analytesNotFound, ", "), "not found.")
+    message("Analytes ", paste(analytesNotFound, ", "), "not found below the FDR cutoff.")
   }
 
   ####################### Get XICs ##########################################
-  runs <- filenames$runs
-  names(runs) <- rownames(filenames)
+
   # Get Chromatogram for each peptide in each run.
   message("Fetching Extracted-ion chromatograms from runs")
-  XICs <- getXICs4AlignObj(dataPath, runs, oswFiles, analytesFound, XICfilter,
-                           SgolayFiltOrd, SgolayFiltLen)
-  names(XICs) <- filenames$runs
+  XICs <- getXICs4AlignObj(mzPntrs, fileInfo, runs, prec2chromIndex, analytes)
+  rm(mzPntrs)
   XICs
 }
--- tests/testthat/test_get_peaks_chromatograms.R
+++ tests/testthat/test_get_peaks_chromatograms.R
@@ -16,49 +16,39 @@ test_that("test_getXICs4AlignObj", {
   dataPath <- system.file("extdata", package = "DIAlignR")
   runs <- c("run1" = "hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
             "run0" =  "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt")
-  filenames <- data.frame("filename" = c("data/raw/hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt.mzML.gz",
-                                         "data/raw/hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt.mzML.gz",
-                                         "data/raw/hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt.mzML.gz"),
-                          "runs" = c("hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt",
-                                     "hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt",
-                                     "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt"),
-                          row.names = c("run0", "run1", "run2"),
-                          stringsAsFactors=FALSE)
-  oswFiles <- getOswFiles(dataPath = dataPath, filenames, maxFdrQuery = 0.05,
-              analyteFDR = 0.01, oswMerged = TRUE, analyteInGroupLabel = FALSE)
-  analytes <- "QFNNTDIVLLEDFQK_3"
-  outData <- getXICs4AlignObj(dataPath = dataPath, runs, oswFiles, analytes,
-                   SgolayFiltOrd = 4, SgolayFiltLen = 13)
+  analytes <- c(32L, 898L, 4618L)
+
+  fileInfo <- getRunNames(dataPath, oswMerged = TRUE)
+  precursors <- getPrecursorByID(analytes,fileInfo)
+  mzPntrs <- getMZMLpointers(fileInfo)
+  prec2chromIndex <- getChromatogramIndices(fileInfo, precursors, mzPntrs)
+  expect_warning(outData <- getXICs4AlignObj(mzPntrs, fileInfo, runs, prec2chromIndex, analytes))
+  rm(mzPntrs)
+
   data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
   XICs <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR
-  expect_identical(names(outData), c("run1", "run0"))
-  expect_identical(names(outData[["run1"]]), "QFNNTDIVLLEDFQK_3")
-  expect_equal(outData[["run0"]][["QFNNTDIVLLEDFQK_3"]], XICs[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
-  expect_equal(outData[["run1"]][["QFNNTDIVLLEDFQK_3"]], XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
+  expect_identical(names(outData), c("hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt","hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt"))
+  expect_identical(names(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]]), c("32", "898", "4618"))
+  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["4618"]], XICs[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
+  expect_equal(outData[["hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt"]][["4618"]], XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
 
-  oswFiles <- getOswFiles(dataPath = dataPath, filenames, maxFdrQuery = 0.05,
-                          analyteFDR = 0.01, oswMerged = TRUE, analyteInGroupLabel = TRUE)
-  outData <- getXICs4AlignObj(dataPath = dataPath, runs, oswFiles, analytes = "14299_QFNNTDIVLLEDFQK/3",
-                              SgolayFiltOrd = 4, SgolayFiltLen = 13)
-  expect_equal(outData[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], XICs[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
-  expect_equal(outData[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], XICs[["run1"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
+  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["32"]], NULL, tolerance = 1e-03)
+  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["898"]], NULL, tolerance = 1e-03)
+  expect_equal(outData[["hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt"]][["32"]], NULL, tolerance = 1e-03)
+  expect_equal(outData[["hroest_K120809_Strep0%PlasmaBiolRepl2_R04_SW_filt"]][["898"]], NULL, tolerance = 1e-03)
+
 })
 
 test_that("test_getXICs", {
   dataPath <- system.file("extdata", package = "DIAlignR")
-  runs <- c("hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt", "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
-  outData <- getXICs(analytes = "QFNNTDIVLLEDFQK_3", runs = runs, dataPath = dataPath,
-          maxFdrQuery = 1.0, SgolayFiltOrd = 4, SgolayFiltLen = 13, runType = "DIA_proteomics",
-          oswMerged = TRUE, nameCutPattern = "(.*)(/)(.*)", analyteInGroupLabel = FALSE)
+  runs <- c("run0" = "hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt",
+            "run2" = "hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt")
+  outData <- getXICs(analytes = 4618L, runs = runs, dataPath = dataPath,
+          maxFdrQuery = 1.0, runType = "DIA_proteomics", oswMerged = TRUE)
   data(XIC_QFNNTDIVLLEDFQK_3_DIAlignR, package="DIAlignR")
   XICs <- XIC_QFNNTDIVLLEDFQK_3_DIAlignR
-  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["QFNNTDIVLLEDFQK_3"]],
+  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["4618"]],
                XICs[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
-  expect_equal(outData[["hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt"]][["QFNNTDIVLLEDFQK_3"]],
+  expect_equal(outData[["hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt"]][["4618"]],
                XICs[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
-  outData <- getXICs(analytes = "14299_QFNNTDIVLLEDFQK/3", runs = runs, dataPath = dataPath,
-                     maxFdrQuery = 1.0, SgolayFiltOrd = 4, SgolayFiltLen = 13, runType = "DIA_proteomics",
-                     oswMerged = TRUE, nameCutPattern = "(.*)(/)(.*)", analyteInGroupLabel = TRUE)
-  expect_equal(outData[["hroest_K120808_Strep10%PlasmaBiolRepl1_R03_SW_filt"]][["14299_QFNNTDIVLLEDFQK/3"]], XICs[["run0"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
-  expect_equal(outData[["hroest_K120809_Strep10%PlasmaBiolRepl2_R04_SW_filt"]][["14299_QFNNTDIVLLEDFQK/3"]], XICs[["run2"]][["14299_QFNNTDIVLLEDFQK/3"]], tolerance = 1e-03)
 })
