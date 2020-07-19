08d33e7423a6b01e4cbf282ece875fa7c2817a1f
shubham1637 <shubham.1637@gmail.com>
2020-07-19 01:48:57 -0400

    [Test] for generating master run with features and XICs

---
+++ tests/testthat/test_merge_runs.R
@@ -0,0 +1,141 @@
+context("Merge two runs")
+
+childFeatures <- function(){
+  df <- data.frame(transition_group_id = 4618L,
+                   feature_id = bit64::as.integer64(c(7675762503084486466, 8133647188324775335, 436075290410024368,
+                                                      1709365655702457288, 7174847956052480089, 8455234228721846034)),
+                   RT = c(5237.8, 5282.2, 5393.2, 5514.4, 5155.9, 4997.2),
+                   intensity = c(229.707813, 0.9945442, 29.670797, 2.622945, 4.2806256, 24.7804246),
+                   leftWidth = c(5217.35, 5278.8, 5365.9, 5497.3, 5137.975, 4980.1),
+                   rightWidth = c(5261.7, 5302.7, 5420.5, 5521.2, 5171.25, 5019.4),
+                   peak_group_rank = c(1L, 2L, 3L, 4L, 5L, 6L),
+                   m_score = c(5.692e-05, 5.039e-02, 0.201, 0.33, 0.36201, 0.36669))
+  df
+}
+
+test_that("test_getNodeRun",{
+  skip_if_no_pyopenms()
+  dir.create("mzml")
+  ropenms <- get_ropenms(condaEnv = envName, useConda=TRUE)
+  dataPath <- system.file("extdata", package = "DIAlignR")
+  params <- paramsDIAlignR()
+  params[["keepFlanks"]] <- TRUE
+  params[["XICfilter"]] <- "none"
+  params[["globalAlignmentFdr"]] <- 0.05
+  fileInfo <- getRunNames(dataPath = dataPath)
+  mzPntrs <- list2env(getMZMLpointers(fileInfo))
+  features <- list2env(getFeatures(fileInfo, maxFdrQuery = 0.05, runType = "DIA_proteomics"))
+  precursors <- data.frame(transition_group_id = 4618L, peptide_id = 14383L,
+                           sequence = "QFNNTDIVLLEDFQK", charge = 3L,
+                           group_label = "14299_QFNNTDIVLLEDFQK/3",
+                           transition_ids	= I(list(27706:27711)))
+  prec2chromIndex <- list2env(getChromatogramIndices(fileInfo, precursors, mzPntrs))
+  mergeName <- "temp"
+  adaptiveRTs <- new.env()
+  refRuns <- new.env()
+
+  expect_message(getNodeRun(runA="run1", runB="run2", mergeName, dataPath = ".",
+                            fileInfo, features, mzPntrs, prec2chromIndex,
+                            precursors, params, adaptiveRTs, refRuns, ropenms),
+                 "Created a child run: temp")
+
+  expect_identical(ls(mzPntrs), c("run0", "run1", "run2", "temp"))
+  expect_is(mzPntrs[["temp"]], "mzRpwiz")
+  expect_equal(features$temp, childFeatures()[1,], tolerance = 1e-04)
+  expect_identical(fileInfo["temp", "chromatogramFile"], "./mzml/temp.chrom.mzML")
+  expect_identical(fileInfo["temp", "runName"], "temp")
+  expect_identical(prec2chromIndex$temp[,"transition_group_id"], 4618L)
+  expect_identical(prec2chromIndex$temp[,"chromatogramIndex"][[1]], 1:6)
+  expect_equal(adaptiveRTs[["run1_run2"]], 77.82315, tolerance = 1e-04)
+  expect_equal(adaptiveRTs[["run2_run1"]], 70.4146, tolerance = 1e-04)
+  expect_identical(refRuns[["temp"]], 1L)
+
+  data(masterXICs_DIAlignR, package="DIAlignR")
+  outData <- mzR::chromatograms(mzR::openMSfile("./mzml/temp.chrom.mzML", backend = "pwiz"))
+  for(i in seq_along(outData)){
+    expect_equal(outData[[i]][[1]], masterXICs_DIAlignR[[1]][[i]][[1]], tolerance = 1e-04)
+    expect_equal(outData[[i]][[2]], masterXICs_DIAlignR[[1]][[i]][[2]], tolerance = 1e-04)
+  }
+  outData <- readRDS("temp_av.rds", refhook = NULL)
+  expect_equal(outData[[1]], masterXICs_DIAlignR[[2]], tolerance = 1e-04)
+  file.remove("temp_av.rds")
+  file.remove(file.path("mzml", "temp.chrom.mzML"))
+  unlink("mzml", recursive = TRUE)
+})
+
+test_that("test_getChildFeature",{
+  data(masterXICs_DIAlignR, package="DIAlignR")
+  newXICs <- masterXICs_DIAlignR
+  params <- paramsDIAlignR()
+  dataPath <- system.file("extdata", package = "DIAlignR")
+  fileInfo <- getRunNames(dataPath = dataPath)
+  features <- getFeatures(fileInfo, maxFdrQuery = 1.00, runType = "DIA_proteomics")
+  df.ref <- features$run1[features$run1$transition_group_id == 4618L, ]
+  df.eXp <- features$run2[features$run2$transition_group_id == 4618L, ]
+  outData <- getChildFeature(newXICs[[1]], newXICs[[2]], df.ref, df.eXp, params)
+
+  df <- childFeatures()
+  expect_equal(outData, df, tolerance = 1e-04)
+})
+
+test_that("test_trfrParentFeature",{
+  data(masterXICs_DIAlignR, package="DIAlignR")
+  newXICs <- masterXICs_DIAlignR
+  timeParent <- newXICs[[2]][, c("tAligned.ref", "alignedChildTime")]
+  colnames(timeParent) <- c("tAligned", "alignedChildTime")
+  params <- paramsDIAlignR()
+  df <- data.frame(transition_group_id = 4618L,
+                   feature_id = bit64::as.integer64(1:5),
+                   RT = c(5238.35, 5395.79, 5519.05, 5022.30, 5054.13),
+                   intensity = c(310.01, 94.8768, 15.8803, 74.6494, 115.591),
+                   leftWidth = c(5220.758, 5367.551, 5500.687, 5005.685, 5036.41),
+                   rightWidth = c(5261.723, 5422.168, 5524.584, 5046.651, 5077.375),
+                   peak_group_rank = c(1L, 2L, 3L, 4L, 5L),
+                   m_score = c(5.692e-05, 0.201, 0.33, 0.367, 0.368))
+  outData <- trfrParentFeature(newXICs[[1]], timeParent, df, params)
+  df[,3:6] <- list(c(5237.8, 5393.2, 5514.4, 4997.2, 5026.2),
+                   c(229.707813, 29.670797, 2.622945, 24.780425, 15.81119),
+                   c(5217.35, 5365.9, 5497.3, 4980.1, 5010.8),
+                   c(5261.7, 5420.5, 5521.2, 5019.4, 5048.4))
+  expect_equal(outData, df, tolerance = 1e-04)
+
+  #TODO: Think about a test where missing value may occur
+})
+
+test_that("test_checkOverlap",{
+  expect_true(checkOverlap(c(1.1, 3.1), c(2.1, 3.1)))
+  expect_true(checkOverlap(c(1.1, 3.1), c(3.1, 4.1)))
+  expect_true(checkOverlap(c(1.1, 3.1), c(2.1, 2.5)))
+  expect_true(checkOverlap(c(1.1, 3.1), c(0.1, 9.1)))
+  expect_false(checkOverlap(c(1.1, 3.1), c(3.2, 7.1)))
+
+})
+
+test_that("test_getChildXICs",{
+  dataPath <- system.file("extdata", package = "DIAlignR")
+  fileInfo <- getRunNames(dataPath = dataPath)
+  features <- getFeatures(fileInfo, maxFdrQuery = 1.00, runType = "DIA_proteomics")
+  mzPntrs <- getMZMLpointers(fileInfo)
+  precursors <- data.frame(transition_group_id = 4618L, peptide_id = 14383L,
+                           sequence = "QFNNTDIVLLEDFQK", charge = 3L,
+                           group_label = "14299_QFNNTDIVLLEDFQK/3",
+                           transition_ids	= I(list(27706:27711)))
+  prec2chromIndex <- getChromatogramIndices(fileInfo, precursors, mzPntrs)
+  refRun <- c(2L)
+  params <- paramsDIAlignR()
+  params[["kernelLen"]] <- 13L
+  params[["polyOrd"]] <- 4L
+  outData <- getChildXICs(runA = "run2", runB="run1", fileInfo, features, mzPntrs, precursors,
+               prec2chromIndex, refRun, params)
+  rm(mzPntrs)
+
+  alignObj <- testAlignObj()
+  expect_identical(dim(outData[[2]][[1]]), c(184L, 5L))
+  expect_identical(colnames(outData[[2]][[1]]), c("indexAligned.ref", "indexAligned.eXp",
+              "tAligned.ref", "tAligned.eXp", "alignedChildTime"))
+  alignObj@indexA_aligned[alignObj@indexA_aligned == 0L] <- NA_integer_
+  expect_identical(alignObj@indexA_aligned, outData[[2]][[1]][,"indexAligned.ref"])
+  alignObj@indexB_aligned[alignObj@indexB_aligned == 0L] <- NA_integer_
+  expect_identical(alignObj@indexB_aligned, outData[[2]][[1]][,"indexAligned.eXp"])
+  expect_equal(outData[[3]], list(ab = 36.8015, ba = 38.6594), tolerance = 1e-04)
+})
